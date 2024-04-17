ad678b90eef772148a268730a2809442b41fffab
wangyat15 <114657394+wangyat15@users.noreply.github.com>
2024-04-17 10:19:55 +0200

    Add files via upload

---
+++ Sprint1_Pair14-main/sprint_1/src/wdc/opt4_wcps_minimum.py
@@ -0,0 +1,26 @@
+# option 4
+def main():
+      print ("option 4")
+      # WCPS query with aggregation operation to retrieve the minimum temperature of Bremen City in 2014
+      import requests
+
+      # Set base url which can be used in further code examples
+      service_endpoint = "https://ows.rasdaman.org/rasdaman/ows"
+      base_wcs_url = service_endpoint + "?service=WCS&version=2.0.1"
+
+      # WCPS query with aggregation operation - minimum
+
+      query = '''
+      for $c in (AvgLandTemp) 
+      return
+        min ( $c[Lat(53.08), Long(8.80),ansi("2014-01-01T00:00":"2014-12-31T18:00")] )
+      '''
+
+      response = requests.post(service_endpoint, data = {'query': query}, verify=False)
+      my_concat_str = "minimum temperature of Bremen City in 2014 : " + str(response.content.decode() + "C")
+      print(my_concat_str)  
+      print("==============================")
+      print(" ")
+   
+if __name__ == "__main__":
+    main()
